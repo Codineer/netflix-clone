d2c0b532ab1172655659b682d74a3594298e6e63
codineer <utkarshgamer294@outlook.com>
2024-04-04 20:23:07 +0530

    commit

---
+++ app/genre/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+type Props = {
+    params: {
+        id: string
+    },
+    searchParams: {
+        genre: string
+    }
+}
+
+const GenrePage = ({ params: { id }, searchParams: { genre } }: Props) => {
+    return (
+        <div>
+            Welcome to the genre with {id} and name {genre}
+        </div>
+    )
+}
+
+export default GenrePage
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="bg-red-500">lets build disney</h1>
+    </main>
+  );
+}
